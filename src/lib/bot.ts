ee2e967dcbc8c7570647b7a261a5d0bfd10c95f7
Nikita Chetverikov <nchetverikov@rbc.ru>
2024-08-23 13:51:01 +0300

    Fix link menu

--- src/lib/bot.ts
+++ src/lib/bot.ts
@@ -6,14 +6,26 @@ const WEBHOOK_URL = `${BASE_PATH}api/telegram-hook?secret_hash=${SECRET_HASH}`
 const BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN!!
 const bot = new Telegraf(BOT_TOKEN)
 
-bot.start((ctx) => {
+bot.start(async (ctx) => {
   const chatId = ctx?.message?.chat?.id || ''
   const text = 'Нажмите кнопку ниже, чтобы открыть Mini App:';
+  const urlApp = BASE_PATH + `?chatId=${chatId}`
 
-  ctx.reply(text,
-    Markup.inlineKeyboard([Markup.button.webApp("Open", BASE_PATH + `?chatId=${chatId}`)]),
+  await ctx.reply(text,
+    Markup.inlineKeyboard([Markup.button.webApp("Open", urlApp)]),
   )
 });
+
+bot.command('menu', (ctx) => {
+  const chatId = ctx?.message?.chat?.id || ''
+  const urlApp = BASE_PATH + `?chatId=${chatId}`
+
+  ctx.setChatMenuButton({
+    text: '🕹 Click',
+    type: 'web_app',
+    web_app: { url: urlApp }
+  })
+})
 
 export const initWebhook = () => {
   return bot.telegram.setWebhook(WEBHOOK_URL)
